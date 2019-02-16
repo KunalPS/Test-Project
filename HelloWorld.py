c704a48edbea318830349e3c0d76dc569af537da
KunalPS <47605966+KunalPS@users.noreply.github.com>
2019-02-16 14:18:57 +0530

    Deleting age functionality

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,11 +3,7 @@
 # raw_input() is used to read textstring form the user in python2 and input() is used to read integer
 # In python 3 input is used to read both string and integer from the user(Every input is condidered as string 'Str()')
 name=input("Your name please!!")
-age= input("and your age..")
 if name!='':
-  if int(age)>50:
-    print("Hello, "+ name + "You are old")
-  else:
-    print("Hello, " + name)
+  print("Hello, " + name)
 else: 
   print("Hello, World")
