41609f92bc29649c538babc184c75312db727cd1
KunalPS <47605966+KunalPS@users.noreply.github.com>
2019-02-16 15:56:46 +0530

    Added name

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,8 @@
 # Python 3
 
 # raw_input() is used to read textstring form the user in python2 and input() is used to read integer
-# In python 3 input is used to read both string and integer from the user(Every input is condidered as string 'Str()')
+# In python 3 input is used to read both string and integer from the user(Every input is condidered as string 'Str()')name
+name=input("Your name please!!")
 age= input("and your age..")
 if name!='':
   if int(age)>30:
